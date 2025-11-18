577b29320c5c9b6d9a028037d0a7166eccff53b1
Johnathan Stowers <j.stowers1@icloud.com>
2025-11-17 19:56:23 -0500

    Changed Project Around
    Redid entire project LOL

---
+++ api/api.py
@@ -0,0 +1,18 @@
+import time
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    print("Hello world!")
+    return jsonify({"message": "hello world!"})
+
+if __name__ == '__main__':
+    app.run(port=5000)
---
+++ src/Insecure.jsx
@@ -0,0 +1,23 @@
+
+export default function LIForm(){
+  const sayHello = async () => {
+    try{
+      const res = await fetch("http://localhost:5000/hello");
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return(
+    <div>
+      <form>
+        <label for = "username">Enter username here pls :3</label> <br />
+        <input type="text" />
+
+        <button onClick = {sayHello} type="submit">Submit here brochado</button> <br />
+      </form>
+    </div>
+  )
+}
